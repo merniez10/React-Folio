8ca73504537785108e6e4c024f2cbe6a9e610264
Merniez10 <merniez10@outlook.com>
2023-12-05 10:09:14 -0700

    added about paragraph

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,7 +13,7 @@ function About() {
       Hi My name is Tommy
         </p>
         <p>
-       Web Developer and
+        Hardworking Software Developer currently enrolled in the University of Utah's Full Stack Flex Web Development Program, continuing to further my skills. I love learning new things and working with others in a team environment. Always make room for improvement and look for ways to challenge myself. Happy to be able to gain more experience through this course.
         </p>
       </div>
     </section>
