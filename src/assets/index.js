7592a3239bbfbdec75a0c7b8e926fc07c2c126af
Merniez10 <merniez10@outlook.com>
2023-12-05 09:38:44 -0700

    added folders, images

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Whadaya } from './projects/whadaya.png';
+export { default as ConcertGroupie } from './projects/concert.jpeg';
+export { default as BookSearch } from './projects/books.jpeg';
+export { default as AllDaLaughs } from './projects/laughs.jpeg';
+export { default as NoteTaker } from './projects/notes.jpeg';
+// export { Avatar } from './projects/image0.jpeg'
